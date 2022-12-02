6db5f6598e89214d4048357e174eb185a51ce3a3
Michiel Borkent <michielborkent@gmail.com>
2022-12-02 11:03:23 +0100

    Retrieve input from AOC_SESSION

--- bb/new_day.clj
+++ bb/new_day.clj
@@ -1,7 +1,16 @@
 (ns new-day
   (:require
+   [babashka.curl :as curl]
    [babashka.fs :as fs]
    [clojure.string :as str]))
+
+(defn strip-leading-zero [day]
+  (str/replace day #"^0" ""))
+
+(defn ensure-leading-zero [day]
+  (if (= 1 (count day))
+    (str "0" day)
+    day))
 
 (defn new-day
   {:org.babashka/cli {:coerce {:day :string
@@ -9,7 +18,9 @@
   [{:keys [year day]
     :or {year "22"}}]
   (let [new-year (str "aoc" year)
-        new-day (str "day" day)
+        day-number day
+        new-day (str "day"
+                     (ensure-leading-zero day-number))
         day01 (slurp (fs/file "src" "aoc22" "day01.cljc"))
         replaced (str/replace day01 "aoc22" new-year)
         replaced (str/replace replaced "day01" new-day)]
@@ -17,10 +28,17 @@
     (fs/create-dirs "resources" new-year)
     (let [new-day-resource (fs/file "resources" new-year (str new-day ".txt"))]
       (if-not (fs/exists? new-day-resource)
-        (fs/copy (fs/file "resources" "aoc22" "day01.txt")
-                 new-day-resource)
-        (println (format "Day %s/%s resource already exists." new-year new-day))))
+        (if-let [session (System/getenv "AOC_SESSION")]
+          (let [input-url (format "https://adventofcode.com/2022/day/%s/input" (strip-leading-zero day-number))]
+            (println "Retrieving input from" input-url)
+            (let [body (:body (curl/get input-url {:headers {"Cookie" (str "session=" session)}}))]
+              (spit new-day-resource body)))
+          (do (println "Tip: set the AOC_SESSION environment variable to the Advent of Code session cookie to retrieve your input automatically!")
+              (println "For now, a copy of the input of day 1 is copied as a stub.")
+              (fs/copy (fs/file "resources" "aoc22" "day01.txt")
+                       new-day-resource)))
+        (println (format "File %s already exists." (str new-day-resource)))))
     (let [new-day (fs/file "src" new-year (str new-day ".cljc"))]
       (if-not (fs/exists? new-day)
         (spit new-day replaced)
-        (println (format "Day %s/%s already exists." new-year new-day))))))
+        (println (format "File %s/%s already exists." new-year new-day))))))
